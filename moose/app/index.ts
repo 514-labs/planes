580cf35f5f38e309a73a911f6f6cc0169b803efb
Olivia Kane <olivia@fiveonefour.com>
2026-04-09 12:34:10 -0400

    add t0

--- moose/app/index.ts
+++ moose/app/index.ts
@@ -1,5 +1,12 @@
-export * from "./connectors/fetch_and_ingest_military_aircraft";
-export * from './apis/aircraftSpeedAltitudeByType';
-export * from './apis/mcp';
-export * from './ingest/ingest';
-export * from './ingest/aircraft_stats_mv';
+// export * from "./connectors/fetch_and_ingest_military_aircraft";
+export * from "./apis/aircraftSpeedAltitudeByType";
+export * from "./apis/mcp";
+export * from "./ingest/ingest";
+export * from "./ingest/aircraft_stats_mv";
+
+import { OlapTable } from "@514labs/moose-lib";
+import { AircraftTrackingData } from "./datamodels/models";
+
+const t1 = new OlapTable<AircraftTrackingData>("t", {
+  version: "0.0",
+});
